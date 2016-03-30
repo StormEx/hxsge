9503fd79b2c384c3d2c2d2ce58ce367d8f9725fd
Vitaliy Kaymachnikov <vitaliyk@playtika.by>
2016-03-30 13:26:05 +0300

    added build.hxml and Test class for base data provider example

---
+++ examples/src/hxsge/examples/dataprovider/Test.hx
@@ -0,0 +1,11 @@
+package hxsge.examples.dataprovider;
+
+class Test {
+	public function new() {
+	}
+
+	public static function main() {
+		trace("begin: data provider example.");
+		trace("end: data provider example.");
+	}
+}
