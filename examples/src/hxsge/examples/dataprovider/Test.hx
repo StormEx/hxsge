366ed58e2294b886b98a9fd5cd439b78a2926d5b
Vitaliy Kaymachnikov <vitaliyk@playtika.by>
2016-03-30 17:25:12 +0300

    fixed code in Test class for data providers

--- examples/src/hxsge/examples/dataprovider/Test.hx
+++ examples/src/hxsge/examples/dataprovider/Test.hx
@@ -1,11 +1,17 @@
 package hxsge.examples.dataprovider;
 
+import hxsge.dataprovider.providers.base.IDataProvider;
+import hxsge.dataprovider.providers.base.BaseDataProviderProxy;
+import hxsge.dataprovider.DataProviderManager;
+import hxsge.dataprovider.providers.base.BaseDataProvider;
+
 class Test {
 	public function new() {
 	}
 
 	public static function main() {
 		trace("begin: data provider example.");
+		DataProviderManager.add(new BaseDataProviderProxy());
 		trace("end: data provider example.");
 	}
 }
