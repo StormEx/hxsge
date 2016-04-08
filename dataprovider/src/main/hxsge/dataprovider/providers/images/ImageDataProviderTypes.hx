21eda1bb36c4cf6f94f1780823150b9c869d3f7b
Vitaliy Kaymachnikov <vitaliyk@playtika.by>
2016-04-08 11:09:07 +0300

    added jpg format reader to ImageDataProviderTypes

--- dataprovider/src/main/hxsge/dataprovider/providers/images/ImageDataProviderTypes.hx
+++ dataprovider/src/main/hxsge/dataprovider/providers/images/ImageDataProviderTypes.hx
@@ -1,5 +1,6 @@
 package hxsge.dataprovider.providers.images;
 
+import hxsge.format.images.formats.jpg.JpgImageReader;
 import hxsge.format.images.ImageReader;
 import hxsge.format.images.formats.jxr.JxrImageReader;
 import hxsge.format.images.formats.png.PngImageReader;
@@ -7,7 +8,8 @@ import hxsge.format.images.formats.png.PngImageReader;
 class ImageDataProviderTypes {
 	static public var readers:Map<String, Class<ImageReader>> = [
 		"png" => PngImageReader,
-		"jxr" => JxrImageReader
+		"jxr" => JxrImageReader,
+		"jpg" => JpgImageReader
 	];
 
 	static public function add(ext:String, cls:Class<ImageReader>) {
--- examples/src/hxsge/examples/dataprovider/Test.hx
+++ examples/src/hxsge/examples/dataprovider/Test.hx
@@ -53,7 +53,7 @@ class Test {
 	public static function main() {
 		var zip_url:String = "https://cvs-stage2-by.stagehosts.com/stage/cs_fb_en/assets/cid_" + Std.string(Date.now().getTime()) + "/assets/paytable_1000.zip";
 		var zip_file:String = "c:/Downloads/bundles/preloader/preloader.zip";
-		var jpg_file:String = "c:/Downloads/10Ten10.jpg";
+		var jpg_file:String = "c:/Downloads/horseshoe_jackpot.jpg";
 		var jpg_url:String = "https://cvs-stage2-by.stagehosts.com/stage/cs_fb_en/assets/cid_" + Std.string(Date.now().getTime()) + "/common/img/og/jackpot/1029_jackpot.jpg";
 		var png_file:String = "c:/Downloads/logo_alt.png";
 		var png_url:String = "https://cvs-stage2-by.stagehosts.com/stage/cs_fb_en/assets/cid_" + Std.string(Date.now().getTime()) + "/common/img/og/jackpot/horseshoe_jackpot.png";
@@ -86,10 +86,10 @@ class Test {
 		batch.add(DataProviderManager.get(new DataProviderInfo(zip_url)));
 		batch.add(DataProviderManager.get(new DataProviderInfo(zip_file)));
 		batch.add(DataProviderManager.get(new DataProviderInfo(jpg_file)));
-		batch.add(DataProviderManager.get(new DataProviderInfo(png_url)));
-		batch.add(DataProviderManager.get(new DataProviderInfo(png_file)));
-		batch.add(DataProviderManager.get(new DataProviderInfo(jxr_file)));
-		batch.add(DataProviderManager.get(new DataProviderInfo(jxr_url)));
+//		batch.add(DataProviderManager.get(new DataProviderInfo(png_url)));
+//		batch.add(DataProviderManager.get(new DataProviderInfo(png_file)));
+//		batch.add(DataProviderManager.get(new DataProviderInfo(jxr_file)));
+//		batch.add(DataProviderManager.get(new DataProviderInfo(jxr_url)));
 		batch.itemFinished.add(function(data:IDataProvider){Log.log((data.errors.isError ? "error" : "success") + ": " + data.info.url);});
 		batch.finished.addOnce(function(_){Log.log("batch finished.");});
 		batch.handle();
