e83d637e1aacf7444890e3482115dbbb346fa364
Vitaliy Kaymachnikov <vitaliyk@playtika.by>
2016-04-08 13:40:04 +0300

    added clear of data

--- format/src/main/hxsge/format/base/BaseReader.hx
+++ format/src/main/hxsge/format/base/BaseReader.hx
@@ -1,6 +1,5 @@
 package hxsge.format.base;
 
-import haxe.io.Bytes;
 import hxsge.core.debug.error.ErrorHolder;
 import hxsge.core.memory.Memory;
 import hxsge.core.signal.Signal.Signal1;
@@ -25,6 +24,7 @@ class BaseReader<T> implements IReader implements IDisposable {
 	public function dispose() {
 		Memory.dispose(finished);
 		errors = null;
+		_data = null;
 	}
 
 	public function read() {
