04368b238cab73dae765cba7efa8089f3c250c42
Vitaliy Kaymachnikov <vitaliyk@playtika.by>
2016-04-14 10:09:57 +0300

    added id property to IAsset

--- assets/src/main/hxsge/assets/data/Asset.hx
+++ assets/src/main/hxsge/assets/data/Asset.hx
@@ -1,10 +1,13 @@
 package hxsge.assets.data;
 
 class Asset implements IAsset {
-	public function new() {
+	public var id(default, null):String;
+
+	public function new(id:String) {
+		this.id = id;
 	}
 
 	public function dispose() {
-
+		id = null;
 	}
 }
--- assets/src/main/hxsge/assets/data/IAsset.hx
+++ assets/src/main/hxsge/assets/data/IAsset.hx
@@ -3,4 +3,5 @@ package hxsge.assets.data;
 import hxsge.core.IDisposable;
 
 interface IAsset extends IDisposable {
+	public var id(default, null):String;
 }
