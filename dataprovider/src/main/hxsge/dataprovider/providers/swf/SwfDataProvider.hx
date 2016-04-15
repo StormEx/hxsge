1088fc31a931532f0bfd616fcb7a38974ef00e2b
Vitaliy Kaymachnikov <vitaliyk@playtika.by>
2016-04-15 18:32:30 +0300

    added dummy SwfDataProvider for all platforms except flash

--- dataprovider/src/main/hxsge/dataprovider/providers/swf/SwfDataProvider.hx
+++ dataprovider/src/main/hxsge/dataprovider/providers/swf/SwfDataProvider.hx
@@ -1,5 +1,8 @@
 package hxsge.dataprovider.providers.swf;
 
+#if flash
+import hxsge.core.platforms.Platforms;
+import hxsge.core.debug.Debug;
 import hxsge.dataprovider.providers.swf.data.SwfMetaData;
 import hxsge.core.utils.meta.Meta;
 import hxsge.dataprovider.data.IDataProviderInfo;
@@ -13,8 +16,12 @@ class SwfDataProvider extends DataProvider {
 	public function new(info:IDataProviderInfo) {
 		super(info);
 
-		if(info.meta.isNotEmpty()) {
-			_meta = Meta.parse(info.meta, SwfMetaData);
+		setMeta(info.meta);
+	}
+
+	public function setMeta(meta:String) {
+		if(meta.isNotEmpty()) {
+			_meta = Meta.parse(meta, SwfMetaData);
 		}
 		else {
 			_meta = new Meta(new SwfMetaData());
@@ -25,3 +32,21 @@ class SwfDataProvider extends DataProvider {
 		finished.emit(this);
 	}
 }
+#else
+import hxsge.dataprovider.data.IDataProviderInfo;
+import hxsge.dataprovider.providers.base.DataProvider;
+import hxsge.core.debug.Debug;
+import hxsge.core.platforms.Platforms;
+
+class SwfDataProvider extends DataProvider {
+	public function new(info:IDataProviderInfo) {
+		super(info);
+	}
+
+	override function prepareData() {
+		Debug.trace("[SwfDataProvider] not implemented for " + Platforms.getCurrentPlatform() + " platform...");
+
+		finished.emit(this);
+	}
+}
+#end
