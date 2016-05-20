f9e38705a13973722e60badc554bd0c4be87dfa8
Vitaliy Kaymachnikov <vitaliyk@playtika.by>
2016-05-20 09:01:04 +0300

    added data type for swf meta data fixed access level of data in meta

--- dataprovider/src/main/hxsge/dataprovider/providers/swf/data/SwfMetaData.hx
+++ dataprovider/src/main/hxsge/dataprovider/providers/swf/data/SwfMetaData.hx
@@ -3,7 +3,7 @@ package hxsge.dataprovider.providers.swf.data;
 import hxsge.forge.ISerializable;
 
 class SwfMetaData implements ISerializable {
-	var data(default, null):Array<SwfMetaDataItem> = [];
+	public var data(default, null):Array<SwfMetaDataItem> = [];
 
 	public function new() {
 	}
--- dataprovider/src/main/hxsge/dataprovider/providers/swf/data/SwfMetaDataItem.hx
+++ dataprovider/src/main/hxsge/dataprovider/providers/swf/data/SwfMetaDataItem.hx
@@ -4,7 +4,7 @@ import hxsge.forge.ISerializable;
 
 class SwfMetaDataItem implements ISerializable {
 	public var name(default, null):String;
-	public var type(default, null):String;
+	public var type(default, null):SwfMetaDataType;
 
 	public function new() {
 	}
