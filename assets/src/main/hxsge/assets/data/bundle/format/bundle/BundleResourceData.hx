3a43a8fd82f51c67b9bf84462b141f61876a9e16
Vitaliy Kaymachnikov <vitaliyk@playtika.by>
2016-06-02 18:33:48 +0300

    added data field for binary resource data

--- assets/src/main/hxsge/assets/data/bundle/format/bundle/BundleResourceData.hx
+++ assets/src/main/hxsge/assets/data/bundle/format/bundle/BundleResourceData.hx
@@ -4,6 +4,7 @@ import hxsge.forge.ISerializable;
 
 class BundleResourceData implements ISerializable {
 	public var name(default, null):String;
+	public var data(default, null):Dynamic;
 	public var meta(default, null):Dynamic;
 
 	public function new() {
