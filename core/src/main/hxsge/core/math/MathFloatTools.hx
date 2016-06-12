29ef131c044a0311693a1214e6f1e05b9b8dee2f
StormEx <stormbox@tut.by>
2016-06-12 21:47:42 +0300

    added format function to MathFloatTools to convert float to string with mantis precision and number separation by ','

--- core/src/main/hxsge/core/math/MathFloatTools.hx
+++ core/src/main/hxsge/core/math/MathFloatTools.hx
@@ -4,4 +4,32 @@ class MathFloatTools {
 	inline public static function clamp(value:Float, min:Float, max:Float):Float {
 		return value < min ? min : (value > max ? max : value);
 	}
+
+	inline public static function format(value:Float, precision:Int = 2):String {
+		if (precision < 0) {
+			precision = 0;
+		}
+		else if (precision > 8) {
+			precision = 8;
+		}
+
+		var res:String = "";
+		var str:String = Std.string(value);
+		var val:Int = str.lastIndexOf('.');
+
+		if (val == -1) {
+			val = str.length - 1;
+		}
+		else {
+			res = str.substr(val, precision + 1);
+		}
+
+		while (val > 2) {
+			res = (val != 3 ? "," : "") + str.substring(val - 3, val) + res;
+			val -= 3;
+		}
+		res = str.substring(0, val) + res;
+
+		return res;
+	}
 }
