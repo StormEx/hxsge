018373355dc38700b73d6f6305bdbb2c26a4b0ef
Vitaliy Kaymachnikov <vitaliyk@playtika.by>
2016-06-16 18:04:11 +0300

    added npm's dependencies

--- format/src/main/hxsge/format/tools/tson/build.bat
+++ format/src/main/hxsge/format/tools/tson/build.bat
@@ -1,8 +1,7 @@
-rmdir /s /q %~dp0..\..\..\..\..\..\build\nodejs\tsonmanager
-xcopy /s /Y %~dp0res  %~dp0..\..\..\..\..\..\build\nodejs
-xcopy /s /Y %~dp0res  %~dp0..\..\..\..\..\..\build\js
-haxe build.hxml -debug -js %~dp0..\..\..\..\..\..\build\nodejs\test.js -D hxnodejs -D nodejs  -lib hxnodejs
-haxe build.hxml -debug -js %~dp0..\..\..\..\..\..\build\js\test.js
-nwbuild -v 0.12.2 -p win64 -o %~dp0..\..\..\..\..\..\build\nodejs %~dp0..\..\..\..\..\..\build\nodejs
-echo %~dp0
+haxe build.hxml -debug -js %~dp0..\..\..\..\..\..\build\tsonmanager\nodejs\test.js -D hxnodejs -D nodejs  -lib hxnodejs
+haxe build.hxml -debug -js %~dp0..\..\..\..\..\..\build\tsonmanager\js\test.js
+rmdir /s /q %~dp0..\..\..\..\..\..\build\tsonmanager\nodejs\tsonmanager
+xcopy /s /Y %~dp0res  %~dp0..\..\..\..\..\..\build\tsonmanager\nodejs
+xcopy /s /Y %~dp0res  %~dp0..\..\..\..\..\..\build\tsonmanager\js
+nwbuild -v 0.12.2 -p win64 -o %~dp0..\..\..\..\..\..\build\tsonmanager\nodejs %~dp0..\..\..\..\..\..\build\tsonmanager\nodejs
 exit
