b244ec1f14059a40476710efc54ba9e9247581b1
StormEx <stormbox@tut.by>
2016-06-20 23:01:22 +0300

    fixed meta data type

--- dataprovider/src/main/hxsge/dataprovider/data/IDataProviderInfo.hx
+++ dataprovider/src/main/hxsge/dataprovider/data/IDataProviderInfo.hx
@@ -3,7 +3,7 @@ package hxsge.dataprovider.data;
 interface IDataProviderInfo {
 	public var url(default, null):String;
 	public var data(default, set):Dynamic;
-	public var meta(default, null):String;
+	public var meta(default, null):Dynamic;
 
 	public var isNeedToLoad(get, never):Bool;
 }
