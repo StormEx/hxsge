a2068ede6ccbae26bcfb5ff2efcadc94a4023e55
Vitaliy Kaymachnikov <vitaliyk@playtika.by>
2016-07-13 08:58:18 +0300

    added code for base loader on any platform (loading from disc only)

--- loaders/src/main/hxsge/loaders/data/DataLoader.hx
+++ loaders/src/main/hxsge/loaders/data/DataLoader.hx
@@ -8,19 +8,43 @@ typedef DataLoader = NodeJsDataLoader;
 typedef DataLoader = JsDataLoader;
 #elseif java
 typedef DataLoader = JavaDataLoader;
-#elseif cpp
-typedef DataLoader = CppDataLoader;
-#elseif cs
-typedef DataLoader = CsDataLoader;
+//#elseif cpp
+//typedef DataLoader = CppDataLoader;
+//#elseif cs
+//typedef DataLoader = CsDataLoader;
 #else
-import hxsge.core.debug.Debug;
+import haxe.io.Bytes;
+import sys.io.FileInput;
 import hxsge.loaders.base.BaseLoader;
 
 class DataLoader extends BaseLoader {
+	var _stream:FileInput;
+
 	public function new(url:String) {
 		super(url);
+	}
 
-		Debug.error("Can't find data loader for this platform...");
+	override function performLoad() {
+		if(url.indexOf("http") == -1) {
+			_stream = sys.io.File.read(url, true);
+			content = _stream.readAll();
+
+			performComplete();
+		}
+		else {
+			performFail(url);
+		}
+	}
+
+	function onLoaded(e:Dynamic) {
+		performComplete();
+	}
+
+	function onErrored(e:Dynamic) {
+		performFail(url);
+	}
+
+	function onProgress() {
 	}
 }
 #end
--- loaders/src/main/hxsge/loaders/data/JavaDataLoader.hx
+++ loaders/src/main/hxsge/loaders/data/JavaDataLoader.hx
@@ -1,18 +1,12 @@
 package hxsge.loaders.data;
 
 #if java
-//import hxsge.loaders.base.BaseLoader;
-//
-//typedef JavaDataLoader = BaseLoader;
 import haxe.io.Bytes;
 import sys.io.FileInput;
 import hxsge.loaders.base.BaseLoader;
-import java.io.NativeInput;
-import java.StdTypes;
 
 class JavaDataLoader extends BaseLoader {
 	var _stream:FileInput;
-//	var _stream:java.io.NativeInput
 
 	public function new(url:String) {
 		super(url);
