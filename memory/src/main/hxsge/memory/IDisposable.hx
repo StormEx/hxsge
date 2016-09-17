49f88116789ed6a7680e2daed7ba996eee6c7749
StormEx <stormbox@tut.by>
2016-09-18 00:04:33 +0300

    added stats for idisposable objects (reverted from commit 413f15a6501d5c37c4095e4a2786ab4c93493f3b)

--- memory/src/main/hxsge/memory/IDisposable.hx
+++ memory/src/main/hxsge/memory/IDisposable.hx
@@ -1,14 +1,5 @@
 package hxsge.memory;
 
-#if !macro
-@:autoBuild(hxsge.memory.macro.DisposableMacro.build())
-#end
-
 interface IDisposable {
 	public function dispose():Void;
-}
-
-
-class DisposableStats {
-	public static var instances:Map<IDisposable, Bool> = new Map();
 }
